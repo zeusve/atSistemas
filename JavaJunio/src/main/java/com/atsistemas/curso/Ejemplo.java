4ed10cf615d7c6ec351c22bcb2dc7a4540893c90
Zeus <zeusgit3@gmail.com>
2022-06-22 16:48:55 +0200

    añado proyecto

---
+++ JavaJunio/src/main/java/com/atsistemas/curso/Ejemplo.java
@@ -0,0 +1,12 @@
+package com.atsistemas.curso;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MavenBean mv = new MavenBean("Bean");
+		mv.getNombre();
+
+	}
+
+}
